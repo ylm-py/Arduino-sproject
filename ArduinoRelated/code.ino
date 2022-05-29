55974a84e4472334de6d7733c89908e6cae6dd26
ylm-py <mr.yassinelemrani@gmail.com>
2022-05-29 15:41:51 +0100

    v2

--- ArduinoRelated/code.ino
+++ ArduinoRelated/code.ino
@@ -1,17 +1,24 @@
  
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Servo.h>
+
  
 #define SS_PIN 10
 #define RST_PIN 9
-const int buzzer = 6;
-const int ledPin = 7;
+const int buzzer = 5;
+const int ledPinGreen = 7;
+const int ledPinRed = 6;
+int pos = 0;
+Servo myservo;
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
  
 void setup() 
 {
-  pinMode(ledPin,OUTPUT);
+  
+  pinMode(ledPinRed,OUTPUT);
+  pinMode(ledPinGreen,OUTPUT);
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
@@ -19,8 +26,12 @@ void setup()
 //  Serial.println();
 
 }
+//card 1 : E9163CE9
+//card 2 : 23CC2099
+//tag 1 : 8905DB3
 void loop() 
 {
+  
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -44,21 +55,25 @@ void loop()
   Serial.println();
   content.toUpperCase();
 //  Serial.print(content);
-  if(content == "23CC2099" || content == "53842794"){
+// A for loop could be a better approach and storing all allowed UIDs inside an array
+  if(content == "23CC2099" || content == "53842794"){ //add allowed cards here
     tone(buzzer, 1000); // Send 1KHz sound signal...
-    digitalWrite(ledPin,HIGH);
+    digitalWrite(ledPinGreen,HIGH);
     delay(500);        // ...for 1 sec
     noTone(buzzer);     // Stop sound...
-    digitalWrite(ledPin,LOW);
+    digitalWrite(ledPinGreen,LOW);
     delay(500);        // ...for 1sec
     
-    content = "";
+    
   }
   else{
     tone(buzzer, 200); // Send 1KHz sound signal...
+    digitalWrite(ledPinRed,HIGH);
     delay(500);        // ...for 1 sec
+    digitalWrite(ledPinRed,LOW);
     noTone(buzzer);     // Stop sound...
   }
+  content = "";
   
   
   delay(800);
